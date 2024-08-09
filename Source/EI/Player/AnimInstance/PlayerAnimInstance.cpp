3ceeaed23878ca8aa3ad8b8d6e20007eae86862a
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-08-09 14:01:51 +0900

    애니메이션 추가

---
+++ Source/EI/Player/AnimInstance/PlayerAnimInstance.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AnimInstance/PlayerAnimInstance.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Pawn = TryGetPawnOwner();
+	if (!Pawn) { return; }
+
+	MovementComponent = Pawn->GetMovementComponent();
+}
+
+void UPlayerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!Pawn) { return; }
+
+	Speed = MovementComponent->Velocity.Length();
+}
